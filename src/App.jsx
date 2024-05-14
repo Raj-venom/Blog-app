ad89ba480c4ded41edf8182dba24cae10a43f0dd
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-05-14 19:39:59 +0545

    configure app and main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
-import conf from './conf/conf.js'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth.js'
+import { login, logout } from './features/auth/authSlice.js'
+import { Footer, Header } from './components/index.js'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
-
-  return (
-
-    <>
-    {console.log(conf.appwriteBucketId)}
-    {console.log(conf.appwriteCollectionId)}
-    <h1>Hello world</h1>
-    </>
-    
+  const [loading, setLoading] = useState(true) // initial loading is true . will change when useEffect job done
+  const dispatch = useDispatch()
 
 
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+
+      })
+      .finally(() => setLoading(false))
+
+  }, [])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <Outlet />
+          test
+        </main>
+        <Footer />
+      </div>
+    </div>
+
+  ) : <div className='flex justify-center items-center h-screen font-semibold text-4xl ' >loading...</div>
+
 }
 
 export default App
